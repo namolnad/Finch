a3e01e5f58b052154ab021907b1d91efef9801e3
Dan L <daniel.h.loman@gmail.com>
2018-08-15 11:32:51 -0700

    Improve file organization. Use router as router and delegate out tasks

--- DiffFormatter/ArgumentRouter.swift
+++ DiffFormatter/ArgumentRouter.swift
@@ -29,42 +29,29 @@ struct ArgumentRouter {
             return
         }
 
-        let patternCreator = FindReplacePatternCreator(configuration: configuration)
-
-        let lines = Output.primaryOutput(for: patternCreator.patterns, with: primaryArg)
-            .components(separatedBy: "\n")
-            .filter { !$0.isEmpty }
-
-        var sections: [String: Section] = [:]
-
-        let reversedSectionInfos = configuration.sectionInfos.reversed()
-
-        lines.forEach { line in
-            if let sectionInfo = reversedSectionInfos.first(where: { $0.tags.contains("*") || $0.tags.contains(where: line.contains) }) {
-                sections[sectionInfo.title, default: Section(info: sectionInfo, lines: [])].lines.append(line)
-            }
-        }
-
         let commandValues = args.compactMap(Argument.commands)
 
         let versionHeader = commandValues
-            .first { $0.command == .version }
+            .first { $0.command == .version }?
+            .value
 
         let releaseManager = commandValues
             .first { $0.command == .releaseManager }
             .flatMap { email in configuration.users.first { $0.email == email.value } }
 
-        output(Output(
-            version: versionHeader?.value,
-            releaseManager: releaseManager,
-            sections: configuration.sectionInfos.compactMap { sections[$0.title] },
-            footer: configuration.footer
-            )
+        let outputGenerator = OutputGenerator(
+            configuration: configuration,
+            rawDiff: primaryArg,
+            version: versionHeader,
+            releaseManager: releaseManager
         )
+
+        output(generator: outputGenerator)
     }
 
-    private func output(_ output: Output) {
-        let result = output.finalOutput
+    private func output(generator: OutputGenerator) {
+        let result = generator.generatedOutput()
+
         pbCopy(text: result)
 
         print("Output copied to pasteboard: \(result)")

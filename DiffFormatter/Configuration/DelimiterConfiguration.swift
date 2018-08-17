e8c5bc3b67bbdbb2b4aa77451a9220bdf847c92e
Dan L <daniel.h.loman@gmail.com>
2018-08-17 11:57:00 -0700

    Add test for custom delimiter output

--- DiffFormatter/Configuration/DelimiterConfiguration.swift
+++ DiffFormatter/Configuration/DelimiterConfiguration.swift
@@ -8,11 +8,25 @@
 
 import Foundation
 
-struct DelimiterConfiguration: Codable {
+struct DelimiterConfiguration: Codable, Equatable {
     let input: DelimiterPair
     let output: DelimiterPair
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        input = (try? container.decode(DelimiterPair.self, forKey: .input)) ?? .empty
+        output = (try? container.decode(DelimiterPair.self, forKey: .output)) ?? .empty
+    }
+
+    init(input: DelimiterPair, output: DelimiterPair) {
+        self.input = input
+        self.output = output
+    }
 }
 
 extension DelimiterConfiguration {
     static let `default`: DelimiterConfiguration = .init(input: .defaultInput, output: .defaultOutput)
+
+    static let empty: DelimiterConfiguration = .init(input: .empty, output: .empty)
 }

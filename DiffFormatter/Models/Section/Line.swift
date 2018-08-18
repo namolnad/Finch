531f20ded3e0c3ae757d0932c7a423cfa7752a88
Dan L <daniel.h.loman@gmail.com>
2018-08-17 20:07:05 -0700

    Remove reversal tag/line parsing

--- DiffFormatter/Models/Section/Line.swift
+++ DiffFormatter/Models/Section/Line.swift
@@ -9,26 +9,39 @@
 import Foundation
 
 struct Line {
-    let tags: Set<String>
+    let tags: [String]
     let value: String
 
     init(configuration: Configuration, value: String) {
         self.value = value
         let outputDelimiters = configuration.delimiterConfig.output
-        self.tags = Set(
-            matches(pattern: "\(outputDelimiters.left.escaped)([^\(outputDelimiters.left.escaped)\(outputDelimiters.right.escaped)]*)\(outputDelimiters.right.escaped)", body: value).compactMap {
-                guard $0.numberOfRanges > 0 else {
-                    return nil
-                }
-                return String(range: $0.range(at: 1), in: value)
+        self.tags = matches(pattern: "\(outputDelimiters.left.escaped)([^\(outputDelimiters.left.escaped)\(outputDelimiters.right.escaped)]*)\(outputDelimiters.right.escaped)", body: value).compactMap {
+            guard $0.numberOfRanges > 0 else {
+                return nil
             }
-        )
+            return String(range: $0.range(at: 1), in: value)
+        }
     }
 }
 
 extension Line {
     func belongsTo(sectionInfo: SectionInfo) -> Bool {
-        return sectionInfo.tags.contains("*") ||
-            !sectionInfo.tags.isDisjoint(with: tags)
+        guard !sectionInfo.tags.contains("*") else {
+            return true
+        }
+
+        return tags.first(where: sectionInfo.tags.contains) != nil
+    }
+
+    func placeInOwningSection(tagToSectionInfo: [String: SectionInfo], titleToSection: inout [String: Section]) {
+        for tag in tags {
+            if let sectionInfo = tagToSectionInfo[tag] {
+                titleToSection[sectionInfo.title, default: .default(for: sectionInfo)].lines.append(value)
+                return
+            }
+        }
+        if let info = tagToSectionInfo["*"] {
+            titleToSection[info.title, default: .default(for: info)].lines.append(value)
+        }
     }
 }

6985d31ad177810b5d71eedc575858c7a7f3b645
Dan L <daniel.h.loman@gmail.com>
2018-11-05 19:23:49 -0800

    [usage] Improve usage format. Use context variables

--- DiffFormatter/Routing/ArgumentHandlers/UsageHandler.swift
+++ DiffFormatter/Routing/ArgumentHandlers/UsageHandler.swift
@@ -11,44 +11,48 @@ import Foundation
 extension ArgumentRouter {
     private static let usageArguments: [String] = ["--help", "-h"]
 
-    static let usageHandler: RouterArgumentHandling = .init { _, args in
+    static let usageHandler: RouterArgumentHandling = .init { context, args in
         guard args.contains(where: usageArguments.contains) else {
             return .notHandled
         }
 
         // NOTE: - Long term, this should defer to sub-handlers for command-specific usage
 
-        print("\(usageInformation)")
+        print(usageInformation(context: context))
 
         return .handled
     }
 
-    private static let usageInformation: String = """
+    private static func usageInformation(context: Context)-> String {
+        return """
 
-DiffFormatter Info:
+        \(context.app.name) Info:
+            -h, --help
+                Displays this dialog
+            -v, --version
+                Displays \(context.app.name) version information
 
--h, --help
-    Displays this dialog
--v, --version
-    Displays DiffFormatter version information
+        Diff Formatting:
+            The first 2 arguments must be (branch or tag) version strings, given as:
+
+            `\(context.app.name) OLD_VERSION NEW_VERSION`
+
+        Diff-Modifying Arguments:
+            --no-show-version
+                The ability to hide the version header
+            --release-manager
+                The release manager's email, e.g. `--release-manager=$(git config --get user.email)`
+            --project-dir
+                Project directory if \(context.app.name) is not being called from project directory
+            --git-diff
+                Manually-passed git diff in expected format. See README for format details.
+
+        Configuration:
+            Configuration instructions available in the README.
 
 
-Typical Usage — DiffFormatting:
+        Additional information available at: https://github.com/namolnad/\(context.app.name)
 
-
-The first two arguments received must be the version strings in the order: OLD_VERSION NEW_VERSION
-Note: - branch or tag values are both acceptable version strings
-
-To modify the output diff, other accepted arguments are:
-
---no-show-version
-    The ability to hide the version header
---release-manager
-    The release manager
---project-dir
-    Project directory if DiffFormatter is not being called from project directory
---git-diff
-    Manual git diff. Must be received in format: git log --left-right --graph --cherry-pick --oneline --format=format:'&&&%H&&& - @@@%s@@@###%ae###' --date=short OLD_VERSION...NEW_VERSION
-
-"""
+        """
+    }
 }

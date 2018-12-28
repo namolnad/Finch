e22be001e9ed324211abe29168edc4ee5b6a0739
Dan L <daniel.h.loman@gmail.com>
2018-12-28 13:17:26 -0800

    Organization tweaks

--- fastlane/Appfile.swift
+++ fastlane/Appfile.swift
@@ -1,7 +1,5 @@
-var appIdentifier: String { return "DiffFormatter" }
-var appleID: String { return environmentVariable(get: "APPLE_ID") }
-
-
-
 // For more information about the Appfile, see:
 //     https://docs.fastlane.tools/advanced/#appfile
+
+var appIdentifier: String { return "DiffFormatter" }
+var appleID: String { return environmentVariable(get: "APPLE_ID") }

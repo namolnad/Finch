4010e22bba20aa1fc2b544d8cb255273f5660d1a
Dan L <daniel.h.loman@gmail.com>
2018-12-29 11:33:10 -0800

    Add log utility; make GitDiffer -> Git utility

--- DiffFormatter/Utilities/Utilities_Git.swift
+++ DiffFormatter/Utilities/Utilities_Git.swift
@@ -1,40 +1,38 @@
 //
-//  Utilities_GitDiffer.swift
+//  Utilities_Git.swift
 //  DiffFormatter
 //
-//  Created by Dan Loman on 8/17/18.
+//  Created by Dan Loman on 12/29/18.
 //  Copyright © 2018 DHL. All rights reserved.
 //
 
 import Foundation
 
 extension Utilities {
-    struct GitDiffer {
-        private let configuration: Configuration
-        private let projectDir: String
-        private let oldVersion: String
-        private let newVersion: String
-
-        init(configuration: Configuration, projectDir: String, oldVersion: String, newVersion: String) {
-            self.configuration = configuration
-            self.projectDir = projectDir
-            self.oldVersion = oldVersion
-            self.newVersion = newVersion
-        }
+    struct Git {
+        let configuration: Configuration
+        let projectDir: String
     }
 }
 
-extension Utilities.GitDiffer {
-    var diff: String {
-        guard !Utilities.isTest else {
-            return ""
-        }
-
+extension Utilities.Git {
+    func diff(oldVersion: String, newVersion: String) -> String {
         return Utilities.shell(
             executablePath: gitExecutablePath,
-            arguments: gitArguments,
+            arguments: gitDiffArguments(
+                oldVersion: oldVersion,
+                newVersion: newVersion
+            ),
             currentDirectoryPath: projectDir
         ) ?? ""
+    }
+
+    func fetch() {
+        _ = Utilities.shell(
+            executablePath: gitExecutablePath,
+            arguments: ["fetch"],
+            currentDirectoryPath: projectDir
+        )
     }
 
     private var gitExecutablePath: String {
@@ -45,14 +43,14 @@ extension Utilities.GitDiffer {
             executablePath: "/bin/bash",
             arguments: ["-c", "which git"],
             currentDirectoryPath: projectDir
-        ) else {
-            return ""
+            ) else {
+                return ""
         }
 
         return path.trimmingCharacters(in: .newlines)
     }
 
-    private var gitArguments: [String] {
+    private func gitDiffArguments(oldVersion: String, newVersion: String) -> [String] {
         return [
             "log",
             "--left-right",

fe804a6a79ddaea6c3b82da1ad9cf418144377f3
Dan L <daniel.h.loman@gmail.com>
2019-01-02 21:05:21 -0800

    Add FormatTemplate for custom default formatTemplate

--- DiffFormatter/Models/Section/Section_Info.swift
+++ DiffFormatter/Models/Section/Section_Info.swift
@@ -20,14 +20,18 @@ extension Section {
 
         let capitalizesMessage: Bool
         let excluded: Bool
-        private let formatString: String
+        let formatTemplate: Section.Line.FormatTemplate?
         let tags: Set<String>
         let title: String
 
-        init(capitalizesMessage: Bool, excluded: Bool = false, formatString: String, tags: [String], title: String) {
+        init(capitalizesMessage: Bool,
+             excluded: Bool = false,
+             formatTemplate: Section.Line.FormatTemplate?,
+             tags: [String],
+             title: String) {
             self.capitalizesMessage = capitalizesMessage
             self.excluded = excluded
-            self.formatString = formatString
+            self.formatTemplate = formatTemplate
             self.tags = Set(tags)
             self.title = title
         }
@@ -35,19 +39,14 @@ extension Section {
 
 }
 
-extension Section.Info {
-    var format: [LineOutputtable] {
-        return formatString.lineOutputtables
-    }
-}
-
 extension Section.Info: Decodable {
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
+        let formatString: String? = container.optionalDecode(forKey: .formatString)
         self.capitalizesMessage = container.decode(forKey: .capitalizesMessage, default: false)
         self.excluded = container.decode(forKey: .excluded, default: false)
-        self.formatString = container.decode(forKey: .formatString, default: .defaultFormatString)
+        self.formatTemplate = Section.Line.FormatTemplate(formatString: formatString)
         self.tags = try container.decode(forKey: .tags)
         self.title = try container.decode(forKey: .title)
     }
@@ -56,14 +55,14 @@ extension Section.Info: Decodable {
 extension Section.Info {
     fileprivate static let `default`: Section.Info = .init(
         capitalizesMessage: false,
-        formatString: .defaultFormatString,
+        formatTemplate: .default,
         tags: ["*"],
         title: "Features"
     )
 
     fileprivate static let bugs: Section.Info = .init(
         capitalizesMessage: false,
-        formatString: .defaultFormatString,
+        formatTemplate: .default,
         tags: ["bugfix", "bug fix", "bug"],
         title: "Bug Fixes"
     )
@@ -74,79 +73,4 @@ extension Array where Element == Section.Info {
         .default,
         .bugs
     ]
-}
-
-private extension String {
-    static let defaultFormatString = " - <<tags>> <<message>> — <<commit_type_hyperlink>> — <<contributor_handle>>"
-
-    // Walk along format string to gather typed LineOutputtable's
-    var lineOutputtables: [LineOutputtable] {
-        var components: [String] = []
-        var component: String = ""
-        if let value = first {
-            component.append(value)
-        }
-
-        func flush() {
-            guard !component.isEmpty else { return }
-            components.append(component)
-            component = ""
-        }
-        func string(for formatComponent: Section.Line.FormatComponent) -> String {
-            return formatComponent.rawValue
-        }
-        func format(component: Section.Line.FormatComponent) -> LineOutputtable {
-            return component
-        }
-
-        for idx in indices.dropFirst().dropLast() {
-            let nextEqualToCurrent = self[index(after: idx)] == self[idx]
-            let currentEqualToPrev = self[index(before: idx)] == self[idx]
-            let currentIsOpening = self[idx] == "<"
-            let currentIsClosing = self[idx] == ">"
-
-            if nextEqualToCurrent && currentIsOpening {
-                flush()
-            }
-
-            component.append(self[idx])
-
-            if currentEqualToPrev && currentIsClosing {
-                flush()
-            }
-        }
-
-        // Clean up last closing
-        if !component.isEmpty {
-            component.append(self[index(before: endIndex)])
-            flush()
-        }
-
-        var transformedComp: [LineOutputtable] = []
-
-        for component in components {
-            let outputtable: LineOutputtable
-
-            switch component {
-            case "<<\(string(for: .commitTypeHyperlink))>>":
-                outputtable = format(component: .commitTypeHyperlink)
-            case "<<\(string(for: .contributorEmail))>>":
-                outputtable = format(component: .contributorEmail)
-            case "<<\(string(for: .contributorHandle))>>":
-                outputtable = format(component: .contributorHandle)
-            case "<<\(string(for: .message))>>":
-                outputtable = format(component: .message)
-            case "<<\(string(for: .tags))>>":
-                outputtable = format(component: .tags)
-            case "<<\(string(for: .sha))>>":
-                outputtable = format(component: .sha)
-            default:
-                outputtable = component
-            }
-
-            transformedComp.append(outputtable)
-        }
-
-        return transformedComp
-    }
 }

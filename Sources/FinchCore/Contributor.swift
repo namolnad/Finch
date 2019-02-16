79983b645a1956c2617689ee284f6dd778d8494b
Dan L <daniel.h.loman@gmail.com>
2019-02-15 17:38:06 -0800

    Allow for either singular email or multiple emails

--- Sources/FinchCore/Contributor.swift
+++ Sources/FinchCore/Contributor.swift
@@ -8,9 +8,41 @@
 
 import Foundation
 
-public struct Contributor: Codable {
+public struct Contributor {
     public let emails: [String]
     public let handle: String
+}
+
+extension Contributor: Codable {
+    enum CodingKeys: String, CodingKey {
+        case email
+        case emails
+        case handle
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        if let emails: [String] = container.optionalDecode(forKey: .emails) {
+            self.emails = emails
+        } else {
+            let email: String = try container.decode(forKey: .email)
+            self.emails = [email]
+        }
+        self.handle = try container.decode(forKey: .handle)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        if emails.count == 1, let email = emails.first {
+            try container.encode(email, forKey: .email)
+        } else {
+            try container.encode(emails, forKey: .emails)
+        }
+
+        try container.encode(handle, forKey: .handle)
+    }
 }
 
 extension Contributor {

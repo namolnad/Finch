ae18cb9441b889e9e543e26cf99ee0d4650d48ea
Dan Loman <daniel.h.loman@gmail.com>
2019-04-15 14:24:00 -0700

    Update test dependencies

--- Tests/Package.swift
+++ Tests/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         // Refer to local Finch package via path
         .package(path: "../"),
         .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.1.0"),
-        .package(url: "https://github.com/jpsim/Yams.git", from: "1.0.1")
+        .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0")
     ],
     targets: [
         .testTarget(

87f9276ed5f96c6b5677cdf95a80c5c1c224eff2
Dan Loman <daniel.h.loman@gmail.com>
2019-04-20 09:19:01 -0700

    Move Versions to own file

--- Sources/FinchApp/Command-Routing/Commands/Compare/CompareCommand.swift
+++ Sources/FinchApp/Command-Routing/Commands/Compare/CompareCommand.swift
@@ -163,21 +163,3 @@ final class CompareCommand: Command {
         )
     }
 }
-
-struct Versions: ArgumentProtocol {
-    let old: Version
-
-    let new: Version
-
-    static var null: Versions = .init(old: .null, new: .null)
-
-    public static var name: String = "versions"
-
-    public static func from(string: String) -> Versions? {
-        guard let versions = try? VersionsResolver().versions(from: string) else {
-            return nil
-        }
-
-        return Versions(old: versions.old, new: versions.new)
-    }
-}

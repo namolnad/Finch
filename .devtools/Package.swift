12894a4c52c69869160e600691a3889f7f12ca6c
Dan Loman <daniel.h.loman@gmail.com>
2020-01-03 14:13:08 -0800

    [platform] Update dependencies

--- .devtools/Package.swift
+++ .devtools/Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
     name: "FinchDevTools",
     dependencies: [
-        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.30.1")
+        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.38.0")
     ],
     targets: [
         .target(name: "FinchDevTools", path: ".", sources: ["Tools.swift"])
